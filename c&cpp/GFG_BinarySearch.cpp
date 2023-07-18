57e91bf667e7cf21bd40ada019232beb6b0309de
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2023-07-19 00:04:10 +0530

    Added GFG_BinarySearch.cpp

---
+++ c&cpp/GFG_BinarySearch.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        int start = 0;
+        int end = n-1;
+        while(start <= end){
+        int mid = start + (end-start)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if(arr[mid]<k){
+                start = mid +1;
+            }
+            else if(arr[mid] > k){
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+   
+     for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
