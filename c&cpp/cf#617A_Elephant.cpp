5b93f837c0ddd5436c3f12c24a2ec0ca8a0d075a
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2023-07-27 00:04:37 +0530

    AAdded cf#617A_Elephant.cpp

---
+++ c&cpp/cf#617A_Elephant.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >>a;
+    if(a<5){
+        cout<<  1;
+    }
+    else if(a%5==0){
+        cout << a/5;
+    }
+    else{
+        cout << a/5 +1;
+    }
+}
