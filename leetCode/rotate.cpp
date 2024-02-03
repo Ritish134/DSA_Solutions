c32c839b8b71d840612b8556c5c05bfdf06f394d
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2024-02-03 23:56:12 +0530

    Create rotate.cpp
    ↻

---
+++ leetCode/rotate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        // Transpose the matrix
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // Reverse each row
+        for (int i = 0; i < n; ++i) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                swap(matrix[i][left], matrix[i][right]);
+                ++left;
+                --right;
+            }
+        }
+    }
+};
