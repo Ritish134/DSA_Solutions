6978d82852bcd40b322728103c6f2afb5af982a0
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2024-02-04 01:16:38 +0530

    Create reverse.cpp

---
+++ leetCode/reverse.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int num = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            
+            // Check for integer overflow ***
+            if (num > INT_MAX / 10 || num < INT_MIN / 10) return 0;
+
+            num = num * 10 + digit;
+            x /= 10;
+        }
+        return num;
+    }
+};
