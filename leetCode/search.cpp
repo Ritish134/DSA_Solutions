9cdb2b76a36a8f6171491855089140308818250c
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2024-02-05 18:53:00 +0530

    Update search.cpp

--- leetCode/search.cpp
+++ leetCode/search.cpp
@@ -3,6 +3,10 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
+        if (nums[start] == target)
+            return start;
+        if (nums[end] == target)
+            return end;
         while(start <= end){
             int mid = start + (end-start)/2;
             if(nums[mid]==target)
