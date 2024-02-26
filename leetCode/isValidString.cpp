01a28fb5eb6adae5cdd300f00c05b7dccccf31eb
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2024-02-26 21:13:01 +0530

    Create isValidString.cpp

---
+++ leetCode/isValidString.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch:s){
+            if(ch=='(' || ch=='{' || ch=='[' ){
+                st.push(ch);
+            }
+            else {
+            if(st.empty() || 
+            (ch==')' && st.top() != '(') ||
+            (ch=='}' && st.top() != '{') ||
+            (ch==']' && st.top() != '[') 
+            ){
+                return false;
+            }
+            st.pop();
+            }   
+        }
+        return st.empty();
+    }
+};
